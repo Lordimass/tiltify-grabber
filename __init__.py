3fd09c6c1116ee32418f67056543d689de49cc7e
Lordimass <sam@tchrin.net>
2024-11-03 16:28:40 +0000

    Google Sheets Integration

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
 from flask import Flask, request
 import threading
 from currency_converter import CurrencyConverter # Used to convert inbound donations to GBP
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+if not(os.path.exists("config.py")):
+    raise Exception('''You are missing config.py, this file is excluded from the GitHub repository
+           as it contains client secrets.''')
+import config
+    
 class DataHandler():
     """
     This class is used as a middleman between `FlaskApplication` and `GSheetsApplication`.
@@ -20,9 +31,15 @@ class DataHandler():
         Arguments:
             port (int) : The port on which to open the `FlaskApplication` on.
         """
-        # Create the `FlaskApplication`, this will start the application and start listening for donations
+        # This will start the application and start listening for donations.
+        # We don't assign an identifier due to the direction of flow of data
+        # FlaskApplication -> DataHandler -> GSheetsApplication
         FlaskApplication(port, self) 
 
+        # This time we do want to assign an identifier since we need to run
+        # its functions at a later point.
+        self.__gSheets = GSheetsApplication()
+        
         self.__donationStack = []
 
     def _push(self, data:dict) -> None:
@@ -53,12 +70,15 @@ class DataHandler():
         if not(eventType == "private:direct:donation_updated" or eventType == "public:direct:donation_updated"):
             return
         
-        # Now we've confirmed that, we need to get the name of the donor, the amount in GBP, and the comment.
+        # Now we've confirmed that, we need to get:
+        # - The name of the donor
+        # - The comment
+        # - The amount in GBP
         datadata = data["data"]
         name = datadata["donor_name"]
         comment = datadata["donor_comment"]
 
-        # To get the amount, we must convert to GBP to keep things consistent
+        # To get the amount, we must convert to GBP to keep currency consistent
         c = CurrencyConverter()
         amount = c.convert(
             datadata["amount"]["value"],
@@ -67,11 +87,8 @@ class DataHandler():
         )
         amount = round(amount, 2)
 
-        # Now we have all the data we need, we can overwrite the big block of data, 
-        # then move to putting the data in GSheets!
-        data = (name, amount, comment)
-        print(data)
-
+        # Now we have all the data we need, we can move to putting the data in GSheets!
+        self.__gSheets.recordDonation(amount, name, comment)
 
 class FlaskApplication(DataHandler):
     """
@@ -108,9 +125,73 @@ class FlaskApplication(DataHandler):
         self.__appThread = threading.Thread(target=self.__app.run, args=(None,self.__port))
         self.__appThread.start()
 
+class GSheetsApplication(DataHandler):
+    """
+    Interfaces with the Google Sheets API in order to write data obtained from the `DataHandler`
+    """
+    
+    def __init__(self):
+        # Authorise and return the GSheet
+        self.__sheet = self.__authorize()
+
+    def __authorize(self) -> any:
+        """
+        Performs Google authorisation process
+
+        The file token.json stores the user's access and refresh tokens, and is
+        created automatically when the authorization flow completes for the first
+        time.
+        """
+
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", config.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", config.SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())#
+        
+        try:
+            service = build("sheets", "v4", credentials=creds)
+            # Call the Sheets API
+            return service.spreadsheets()
+        except HttpError as e:
+            print(e)
+    
+    def recordDonation(self, amount:int, name:str, comment:str = None) -> None:
+        """
+        Records a donation to the sheet
+
+        Arguments:
+            amount (int) : The amount of money donated, in GBP
+            name (str) : The name of the donor
+            comment (str) : The comment attached to the donation, if there was one
+        """
+
+        # The body of the request to the Google Sheet to append
+        body = {
+            "values": [[amount, name, comment]]
+        }
+
+        # Send 
+        self.__sheet.values().append(
+            spreadsheetId = config.SPREADSHEET_ID,
+            range = "Donation List!A2:C2",
+            valueInputOption = "USER_ENTERED",
+            body = body,
+        ).execute()
 
 
 if __name__ == "__main__":
-    DataHandler(port=12345) # Instantiate the DataHandler, beginning webhook receiving and processing
-
-
+    DataHandler(port=config.PORT) # Instantiate the DataHandler, beginning webhook receiving and processing
